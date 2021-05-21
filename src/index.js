b3d44ffa724bb5849140392112c81d150c2a866f
Leandro Milagre Loureiro <leandro.loureiro@solutis.com.br>
2021-05-20 21:08:09 -0300

    1 - Criação do app

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/* Desenvolvido por Leandro M. Loureiro */
+/* Site - www.devleandro.com.br */
+/* Linkedin - https://www.linkedin.com/in/leandro-loureiro-dev/ */
+/* Github - https://github.com/leandromltec */
+
+
+const express = require('express');
+
+const app = express();
+
+//app.listen(4030);
+
+const PORT = process.env.PORT || 4030;
+
+app.listen(PORT, () => {
+    console.log("Iniciada")
+})
+
